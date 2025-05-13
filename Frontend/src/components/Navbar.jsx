0a24951a9cfbfb75ea3817d246b7d9150d864f11
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-05-13 20:55:07 +0545

    navbar added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,19 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="bg-teal-600 text-white px-6 py-4 shadow-md w-full m-0">
       <div className="flex justify-between items-center w-full">
-        <h1 className="text-xl font-bold">Tuition M.S</h1>
-        <ul className="flex space-x-6">
-          <li>
-            <a href="#" className="hover:text-gray-200">Home</a>
+        <Link to="/">
+          <h1 className="text-xl font-bold font-serif tracking-widest">T U T O R I A</h1>
+        </Link>
+
+        <ul className="flex space-x-6 items-center">
+             <li>
+            <Link
+              to="/"
+              className="hover:text-gray-200"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <a href="#" className="hover:text-gray-200">About</a>
+            <Link
+              to="/about"
+              className="hover:text-gray-200"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <a href="#" className="hover:text-gray-200">Contact</a>
+            <Link
+              to="/contact"
+              className="hover:text-gray-200"
+            >
+              Contact
+            </Link>
+          </li>
+           {/* <li>
+            <Link
+              to="/login"
+              className="hover:text-gray-200"
+            >
+              Login
+            </Link>
+          </li>
+           <li>
+            <Link
+              to="/"
+              className="hover:text-gray-200"
+            >
+              Register
+            </Link>
+          </li> */}
+
+          <li>
+            <Link
+              to="/login"
+              className="bg-white text-teal-600 font-semibold px-4 py-1 rounded hover:bg-gray-100 transition"
+            >
+              Login
+            </Link>
+          </li> 
+          <li>
+            <Link
+              to="/register"
+              className="bg-white text-teal-600 font-semibold px-4 py-1 rounded hover:bg-gray-100 transition"
+            >
+              Register
+            </Link>
           </li>
         </ul>
       </div>
